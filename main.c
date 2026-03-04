d1707fe038249a5dd6760278f0eee68f8123e47f
clemgbpro <clementgb.pro@gmail.com>
2026-03-04 21:00:15 +0000

    dfg

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-    printf("Hello World\n");
+    printf("\nWelcome to Inventory-Manager\n\n");
+    printf("Defining I1")
+    struct item i1;
+
     return 0;
+}
+
+struct item{
+    int Id = 0;
+    char Name[255] = 0;
+    int Category = 0;
 }
